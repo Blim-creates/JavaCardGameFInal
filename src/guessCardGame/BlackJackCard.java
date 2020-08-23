14277815b225c89f6244a062b869755e20b5a8c0
Blim-creates <brandonbal76@gmail.com>
2020-08-23 09:52:45 -0700

    Update BlackJackCard.java

--- src/guessCardGame/BlackJackCard.java
+++ src/guessCardGame/BlackJackCard.java
@@ -1,21 +1,25 @@
 package guessCardGame;
 
+//Card Class
 public class BlackJackCard {
 
     private int cardNumber;
     private int rank;
     private String front;
-
+    
+    //Method to define card number, rank, and front
     BlackJackCard(int cardNumber, int rank, String front) {
         this.cardNumber = cardNumber;
         this.rank = rank;
         this.front = front;
     }
 
+    //Boolean for ACE
     public boolean isAce() {
         return rank == 0;
     }
 
+    //Ranking
     public int rank() {
         if (rank == 0) {
             return 1;
@@ -26,6 +30,7 @@ public class BlackJackCard {
         return rank + 1;
     }
 
+    //String of card
     public String toString() {
         return this.front;
     }
